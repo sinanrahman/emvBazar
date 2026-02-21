dbdb0d54d1275b2734db889d5ebfa2e94dd87eea
sinansrk <sinansrk966@gmail.com>
2026-02-21 14:52:43 +0530

    Fix conflict markers in manifest and sw.js, clean up redundant PWA scripts

--- public/sw.js
+++ public/sw.js
@@ -1,54 +1,23 @@
-<<<<<<< HEAD
-const CACHE_NAME = 'emv-bazar-v1';
-const urlsToCache = [
-    '/',
-    '/partials/navbar',
-    // Add other critical assets here
-];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
-});
-=======
 const CACHE_NAME = "emvBazar-cache-v1";
 const urlsToCache = [
-  "/",
-  "/manifest.json",
-  "/image/icon-192x192.png",
-  "/image/icon-512x512.png",
-  // add other static assets
+    "/",
+    "/manifest.json",
+    "/image/icon-192x192.png",
+    "/image/icon-512x512.png"
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((resp) => {
-      return resp || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then((resp) => {
+            return resp || fetch(event.request);
+        })
+    );
 });
->>>>>>> 5a11d4f8de4cf541bfa150114d4353dcd0ae1f91
